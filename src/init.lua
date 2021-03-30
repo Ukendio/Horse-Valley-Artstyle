5fd1ac421f9f1c36a58112d6637832545a55da18
Marcus <ukendio@gmail.com>
2021-03-30 14:53:00 +0200

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -3,6 +3,7 @@
 local House = workspace.Camera:FindFirstChildOfClass("Model")
 local Invalids = {}
 local WALL_HEIGHT = 12
+local MULTIPLE = 0.125
 
 
 -- Check organization
@@ -59,8 +60,10 @@ for _, v in pairs(House:GetDescendants()) do
 	if v:IsA("BasePart") then
 
 		v.Material = Enum.Material.SmoothPlastic
-		v.CanCollide = true
-		v.CastShadow = (v.Parent.Name == "Ceiling" or (v.Parent.Name == "walls" and v.Parent.Parent == Exterior))
+		if v.Name ~= "Knob" then
+			v.CanCollide = true
+		end
+		v.CastShadow = (v.Parent.Name == "Ceiling" or v.Parent.Name == "roof" or (v.Parent.Name == "walls" and v.Parent.Parent == Exterior))
 
 		if v.Name == "Glass" then
 			v.Color = Color3.fromRGB(255, 255, 255)
@@ -68,7 +71,7 @@ for _, v in pairs(House:GetDescendants()) do
 			v.Transparency = 0.75
 		end
 
-		if v.Parent.Name == "Ceiling" or v.Parent.Name == "Walls" and v.Parent.Parent == Interior then
+		if v.Parent.Name == "Ceiling" or v.Parent.Name == "Floor" or (v.Parent.Name == "Walls" and v.Parent.Parent.Parent == Interior) then
 			v.Color = Color3.fromRGB(255, 255, 255)
 		end
 	end
@@ -79,7 +82,7 @@ end
 local numTransposedParts = 0
 local transposingWalls = {}
 for _, room in pairs(Interior:GetChildren()) do
-	for _, v in pairs(room.Floor:GetChildren()) do
+	for _, v in pairs(room.Walls:GetChildren()) do
 		if v.ClassName == "Part" then
 			table.insert(transposingWalls, v)
 		end
@@ -116,6 +119,24 @@ end
 print("Number of transposed parts:", numTransposedParts)
 
 
+-- Make interior walls face front
+local numWallsFacedWrong = 0
+local raycastParams = RaycastParams.new()
+raycastParams.FilterType = Enum.RaycastFilterType.Whitelist
+raycastParams.IgnoreWater = true
+raycastParams.FilterDescendantsInstances = {Exterior.walls}
+for _, room in pairs(Interior:GetChildren()) do
+	for _, wall in pairs(room.Walls:GetChildren()) do 
+		local result = workspace:Raycast(wall.Position, wall.CFrame.LookVector, raycastParams)
+		if result then
+			wall.CFrame = wall.CFrame*CFrame.Angles(0, math.pi, 0)
+			numWallsFacedWrong = numWallsFacedWrong + 1
+		end
+	end
+end
+print("Number of walls faced wrong:", numWallsFacedWrong)
+
+
 -- Check door size
 local doorPaintable = Door:FindFirstChild("Paintable")
 if not doorPaintable then
@@ -136,14 +157,14 @@ end
 -- Check walls size
 for _, v in pairs(Exterior.walls:GetChildren()) do
 	local size = v.Size
-	if (size.Y > 10 and math.round(size.Y) ~= WALL_HEIGHT) or math.round(v.Size.Z*100) ~= 25 then
+	if (size.Y > 10 and math.round(size.Y*100) ~= WALL_HEIGHT*100) or math.round(v.Size.Z*100) ~= 25 then
 		table.insert(Invalids, v)
 	end
 end
 for _, room in pairs(Interior:GetChildren()) do
 	for _, v in pairs(room.Walls:GetChildren()) do
 		local size = v.Size
-		if (size.Y > 10 and math.round(size.Y) ~= WALL_HEIGHT) or math.round(v.Size.Z*100) ~= 25 then
+		if (size.Y > 10 and math.round(size.Y*100) ~= WALL_HEIGHT*100) or math.round(v.Size.Z*100) ~= 25 then
 			table.insert(Invalids, v)
 		end
 	end
@@ -157,21 +178,56 @@ end
 
 -- Check ceiling and floor size
 for _, v in pairs(Ceiling:GetChildren()) do
-	if math.round(v.Size.Y*1000) ~= 125 then
+	if math.round(v.Size.Y*1000) ~= MULTIPLE*1000 then
 		table.insert(Invalids, v)
 	end
 end
 for _, room in pairs(Interior:GetChildren()) do
 	for _, v in pairs(room.Floor:GetChildren()) do
-		if math.round(v.Size.Y*1000) ~= 125 then
+		if math.round(v.Size.Y*1000) ~= MULTIPLE*1000 then
 			table.insert(Invalids, v)
 		end
 	end
 end
 if #Invalids > 0 then
 	game.Selection:Set(Invalids)
-	warn("Floor and Ceiling must be 0.125 height")
+	warn("Floor and Ceiling must be .. " .. MULTIPLE .. " height")
 	return
+end
+
+
+-- Check if any part is lower than floor
+local lowestFloorYPos = nil
+for _, room in pairs(Interior:GetChildren()) do
+	for _, v in pairs(room.Floor:GetChildren()) do
+		local floorYPos = v.Position.Y - v.Size.Y*0.5
+		if not lowestFloorYPos or floorYPos < lowestFloorYPos then
+			lowestFloorYPos = floorYPos
+		end
+	end
+end
+for _, v in pairs(House:GetDescendants()) do
+	if v:IsA("BasePart") and v.Parent.Name ~= "Floor" and v.Name ~= "Base" then
+		local dim = v.Size / 2
+		local corners = {
+			v.CFrame * CFrame.new(dim.X, dim.Y, dim.Z),
+			v.CFrame * CFrame.new(dim.X, dim.Y, -dim.Z),
+			v.CFrame * CFrame.new(dim.X, -dim.Y, dim.Z),
+			v.CFrame * CFrame.new(dim.X, -dim.Y, -dim.Z),
+			v.CFrame * CFrame.new(-dim.X, dim.Y, dim.Z),
+			v.CFrame * CFrame.new(-dim.X, dim.Y, -dim.Z),
+			v.CFrame * CFrame.new(-dim.X, -dim.Y, dim.Z),
+			v.CFrame * CFrame.new(-dim.X, -dim.Y, -dim.Z)
+		}
+		local margin = MULTIPLE*0.1
+		for _, corner in ipairs(corners) do
+			if corner.Position.Y + margin < lowestFloorYPos then
+				game.Selection:Set({v})
+				warn("A part is lower than floor!")
+				return
+			end
+		end
+	end
 end
 
 
@@ -185,10 +241,26 @@ if not Base then
 	Base.CanCollide = false
 	Base.Anchored = true
 	Base.CastShadow = false
-	Base.Transparency = 125
+	Base.Transparency = 1
 	Base.Material = Enum.Material.SmoothPlastic
 	Base.CFrame = modelCFrame - Vector3.new(0, modelSize.Y*0.5 + 0.5, 0)
+	Base.Orientation = doorPaintable.Orientation
+	Base.CFrame = Base.CFrame*CFrame.Angles(0, math.pi, 0)
 	Base.Size = Vector3.new(2, 1, 2)
+end
+House.PrimaryPart = Base
+
+
+-- Rotate floors to Base orientation
+for _, room in pairs(Interior:GetChildren()) do
+	for _, v in pairs(room.Floor:GetChildren()) do
+		if math.round(v.CFrame.LookVector.X) ~= 0 then
+			local size = v.Size
+			v.Size = Vector3.new(size.Z, size.Y, size.X)
+			numTransposedParts = numTransposedParts + 1
+		end
+		v.Orientation = Base.Orientation
+	end
 end
 
 
@@ -217,63 +289,8 @@ for _, room in pairs(Interior:GetChildren()) do
 	end
 end
 
-for _, v in pairs(workspace.Camera:GetDescendants()) do
-    if v:IsA("BasePart") then
-        local size = v.Size
-        size = size*1000
-        if math.floor(size.X%125) ~= 0 or math.floor(size.Y)%125 ~= 0 or math.floor(size.Z)%125 ~= 0 then
-            print('a:', size.X%125, size.Y%125, size.Z%125)
-            table.insert(Invalids, v)
-        end
-    end    
-end
-if #Invalids > 0 then
-    game.Selection:Set(Invalids)
-else
-    print('Great! No invalid size found.')
-end
-
-local Parts = {}
-local Selections = {}
-
-local function RoundVector3(vector3)
-    return Vector3.new(
-        math.floor(vector3.X*1000 + 0.5)*0.001, 
-        math.floor(vector3.Y*1000 + 0.5)*0.001, 
-        math.floor(vector3.Z*1000 + 0.5)*0.001
-    )
-end
-
-for _, v in pairs(workspace.Camera:GetDescendants()) do
-    if v:IsA("BasePart") then
-        table.insert(Parts, {
-            v,
-            RoundVector3(v.Position),
-            RoundVector3(v.Size),
-            RoundVector3(v.Orientation),
-        })
-    end
-end
-
-for i = #Parts, 1, -1 do
-    local part1 = Parts[i]
-    for _, part2 in pairs(Parts) do
-        if part1[1] ~= part2[1] then
-            if part1[2] == part2[2]
-            and part1[3] == part2[3]
-            and part1[4] == part2[4] then
-                print('found duplicate')
-                table.insert(Selections, part2[1])
-                table.remove(Parts, i)
-            end
-        end
-    end
-end
-
-game.Selection:Set(Selections)
 
 -- Finish
+warn("Remember to run _SelectDuplicateParts, _SelectInvalidSizedParts and check for clipping walls")
 warn("THIS SCRIPT DO NOT REPLACE MANUAL REVIEW")
 print('Success! This house looks great :)')
-
-return {}
